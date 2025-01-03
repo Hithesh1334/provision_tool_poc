3fb789dabf41375af7ead4f06d7a38359fe85bb8
Hithesh Patel <hpatel@phdata.io>
2025-01-03 14:19:55 +0530

    added yaml convertor code

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ import requests
 from pandas import read_csv
 import time
 import json
+from yaml_convertor import warehouse_yaml
+import yaml
 
-# st.set_page_config(layout="wide")
-st.set_page_config(page_title="Provision Tool", page_icon=":shield:")
+st.set_page_config(page_title="Provision Tool", page_icon=":shield:",layout='wide')
 
 with open('style.css') as f:
     css = f.read()
@@ -23,6 +24,8 @@ if 'domains' not in st.session_state:
     st.session_state['domains'] = False
 if 'envs' not in st.session_state:
     st.session_state['envs'] = False
+if 'warehouse_df' not in st.session_state:
+    st.session_state['warehouse_df'] = True
 if 'status' not in st.session_state:
     st.session_state['status'] = [False,False,False,False,False,False,False]
 if 'state' not in st.session_state:
@@ -45,43 +48,41 @@ def main():
     st.title("phData Provision Tool")
 
     with st.container(border=True,key="first_block"):
-        st.write("Project Name?")
-        project_name = st.text_input(label='',placeholder="project name... ",disabled=not st.session_state["project_name"],key="project_text_input")
+        project_name = st.text_input(label="Project Name ?",placeholder="project name... ",disabled=not st.session_state["project_name"],key="project_text_input")
         if project_name:
             st.session_state["domains"] = True
             st.session_state["project_name"] = False
+
         st.divider()
 
-        st.write("What are the domains to be included?")
         with open("domains.txt","r") as file:
             domains = [line.strip() for line in file]
-        domain_list = st.pills(label="domains",selection_mode="multi",options=domains,disabled=not st.session_state['domains'],default=None) #change this to st.selector if required
+        domain_list = st.pills(label="domains to be included?",options=domains,disabled=not st.session_state['domains'],default=None,selection_mode="multi",key="domains_selector_key") #change this to st.selector if required
         if domain_list and project_name:
             st.session_state["envs"] = True
             st.session_state["domains"] = False
 
         st.divider()
         
-        # st.write("What are the environments/database to be created on Snowflake?")
         cols = st.columns(2)
-        env = st.radio(
-            "What are the environments/database to be created on Snowflake?",
-            ["Standard", "Customized"],
-            horizontal=True
+        on = st.toggle(
+            "environments/database to be created on Snowflake?",
         )
-        if env == "Standard":
+        env_list = []
+        if on:
             st.session_state['envs'] = False
             env_list = st.pills(label="",selection_mode="multi",options=["PROD","DEV","QA","NONPROD","SANDBOX"],disabled= st.session_state['envs'],default=None)
-        elif env == "Customized":
-            st.session_state['envs'] = True
-            # env_list = st.pills(label="envs",selection_mode="multi",options=["PROD","DEV","QA","NONPROD","SANDBOX"],disabled= st.session_state['envs'],default=None)
-            st.write("database creation code comes ther ")
+        # elif env == "Customized":
+        #     st.session_state['envs'] = True
+        #     # env_list = st.pills(label="envs",selection_mode="multi",options=["PROD","DEV","QA","NONPROD","SANDBOX"],disabled= st.session_state['envs'],default=None)
+        #     st.write("database creation code comes ther ")
 
         st.divider()
 
         if st.button("Next",key="first_block_button"):
             if project_name and domain_list:
                 st.session_state['status'][0] = True
+                st.session_state['warehouse_df'] = False
 
     with st.status(label="Please specify the warehouse that needs to be created",expanded=st.session_state['status'][0],state='complete' if st.session_state['state'][0] else 'error') as warehouse_container:
         warehouse_data = pd.read_json("json/warehouse.json")
@@ -113,11 +114,13 @@ def main():
                         "STANDARD",
                     ],
                     required=True,
+                    default="X-small"
                 ),
-            },hide_index=True,use_container_width=True,num_rows="dynamic")
+            },hide_index=True,use_container_width=True,num_rows="dynamic",disabled=st.session_state['warehouse_df'])
         
         # resource monitor
         rm_required = st.checkbox(label="Do you need resource monitor?")
+        rm_name,rm_monitor_type,rm_frequency,rm_notify,rm_notify_suspend,rm_notify_only= "","","","","",""
         if rm_required: 
             rm_name = st.text_input(label = "Resource monitor name",placeholder="load_monitor",key="resource_monitor")
             rm_monitor_type = st.pills(label = "Monitor type",options=['Account','Warehouse'],key="monitor_type")
@@ -147,31 +150,31 @@ def main():
         radio_value = st.checkbox(label="do you need all the combinations of env and domains?",)
         roles_list = {"role_name":[]}
         if radio_value :
-            cols = st.columns(2)
-            with cols[0]:
-                suffix = st.checkbox(label="Add as suffix?",key="suffix")
-            with cols[1]:
-                prefix = st.checkbox(label="Add as Prefix?",key="prefix")
+            append_on = st.radio(
+                label = "",
+                options = ["Suffix","Prefix"],
+                horizontal = True
+            )
+            
             domain_name_included = st.checkbox(label="Include domain names in object names?",key="domain_radio")               
                 
             envs_name_included = st.checkbox(label="Include env names in object names?",key="envs_radio")
 
             rw_ro = st.checkbox(label="do you want rw_ro combination as well for each role",)
-            with open("json/roles.json", 'r') as file:
+            with open("json/roles.json") as file:
                 roles_data = json.load(file)
             
             if domain_name_included:
                 for role in roles_data:
                     for domain in domain_list:
                         for env in env_list:
-                            print(domain,domain_list,role,"line 198")
                             role_name = role['role_name']
-                            if suffix:
+                            if append_on == "Suffix":
                                 if envs_name_included:
                                     role_name = role_name + '_' + domain + '_' + env
                                 else:
                                     role_name = role_name + '_' + domain 
-                            if prefix:
+                            if append_on == "Prefix":
                                 if envs_name_included:
                                     role_name = env + '_' + domain + '_' + role_name
                                 else:
@@ -214,9 +217,7 @@ def main():
             st.session_state["rows"].append({"user_name": "", "password": "", "roles": []})
         def delete_row(index):
             st.session_state["rows"].pop(index)
-        user_name = []
-        password = []
-        role_to_assg = []
+        user = []
         def render_rows():
             for index, row in enumerate(st.session_state["rows"]):
                 cols = st.columns(4)
@@ -253,43 +254,63 @@ def main():
                         if st.button("Del", key=f"delete_{index}"):
                             delete_row(index)
                             st.rerun()  # Force rerun to update the UI
-                user_name.append(row["user_name"])
-                password.append(row["password"])
-                role_to_assg.append(row["roles"])
+                user.append([row["user_name"],row["password"],row["roles"]])
         
 
         render_rows()
 
+
+
     if st.button("Save Data"):
-        data = {
+        snowflake_config = {
             "Snowflake": {
                 "ProjectName": project_name,
                 "user": [
                     {
-                        "user_name": user_name,
-                        "password": password,
-                        "roles": role_to_assg
-                    }
+                        "user_name": u[0],
+                        "password": u[1],
+                        "roles_to_assign": u[2]
+                    } for u in user
                 ],
-                "warehouse": warehouse_data["warehouse_name"].tolist(),
-                "env": [{"databases": env_list}],
-                "Domains": ["marketing", "finance", "sales"],
-                "roles": roles_data_new["role_name"],
+                "warehouse": [
+                    {
+                        "warehouse_name":warehouse_df["warehouse_name"][i],
+                        "warehouse_size":warehouse_df["warehouse_size"][i],
+                        "warehouse_type":warehouse_df["warehouse_type"][i],
+                        "initially_suspended":str(warehouse_df["initially_suspended"][i])
+                    } for i in range(len(warehouse_df))
+                ],
+                "resource_monitor": {
+                    "resource_monitor_name":"" if not rm_name else rm_name,
+                    "frequency":"" if not rm_frequency else rm_frequency,
+                    "monitor_level":"" if not rm_monitor_type else rm_monitor_type,
+                    "trigger": {
+                        "notify" :"" if not rm_notify else rm_notify,
+                        "notify_suspend":"" if not rm_notify_suspend else rm_notify_suspend,
+                        "notify_only":"" if not rm_notify_only else rm_notify_only
+                    }
+                },
+                "env": env_list,
+                "Domains": domains,
+                "roles": roles_list["role_name"]
             }
         }
 
         with open("output.json", "w") as json_file:
-            json.dump(data, json_file, indent=2)
-        
-    if st.session_state['database_object']:
-        with st.status(label="Please specify the Users to be created",expanded=st.session_state['status'][3],state='complete' if st.session_state['state'][3] else 'error'):
-            st.write("here")
+            json.dump(snowflake_config, json_file, indent=2)
 
-    st.divider()
+        st.divider()
     
-    with st.expander(label="Review",expanded=st.session_state["status"][5]) as review:
-        st.write("review section")
+        with st.expander(label="Review",expanded=True) as review:
+            with open("output.json","r") as file:
+                data = json.load(file)
+            st.json(data,expanded=True)
 
+    if st.button("Convert_json",key="json_to_ymal"):
+        warehouse_yaml()
+        with open("groups\\warehouse.yaml") as file:
+            yaml_data = file.read()
+        st.code(yaml_data,language='yaml',wrap_lines=True,line_numbers=True)
 
 if __name__ == '__main__':
     main()
---
+++ yaml_convertor.py
@@ -0,0 +1,222 @@
+import pandas as pd
+import yaml
+import streamlit as st
+import json
+
+
+savepath = "groups\\"
+file_path = "output.json"
+
+def convert(file,file_name):
+    # Convert the dictionary to YAML
+    yaml_data = yaml.dump(file, default_flow_style=False)
+    path = f"{savepath}{file_name}.yaml"
+    print("LINE 14 in helper.py",path)
+    with open(path, "w") as file:
+        file.write(yaml_data)
+
+def warehouse_yaml():
+    with open(file_path, 'r') as file:
+        json_data = json.load(file)
+    warehouses = []
+
+    # Populate the list with dictionaries
+    for warehouse in json_data['Snowflake']['warehouse']:
+        
+        temp = {
+            'name': warehouse['warehouse_name'] ,
+            'warehouse_size': warehouse['warehouse_size'],
+            'warehouse_type': warehouse['warehouse_type'],
+            'initially_suspended': warehouse['initially_suspended'],
+        }
+        warehouse = {}
+        for key, value in temp.items():
+            if value not in ('', None):
+                warehouse[key] = value
+        warehouses.append(warehouse)
+
+    output_data = {'entries': warehouses}
+    convert(output_data,'warehouse')
+
+def database_yaml(df):
+    df = pd.DataFrame(df)
+    databases = []
+
+    # Populate the list with dictionaries
+    for index, row in df.iterrows():
+        temp = {
+            'name': row['Database_name'],
+            }
+        database = {}
+        for key,value in temp.items():
+            if temp[key] != '' and temp[key] != None and temp[key] != None:
+                database[key] = value
+        databases.append(database)
+
+    output_data = {'entries': databases}
+    convert(output_data,'database')
+    
+def user_yaml(df):
+    df = pd.DataFrame(df)
+    users = []
+
+    # Populate the list with dictionaries
+    for index, row in df.iterrows():
+        temp = {
+            'name': row['user_name'],
+            'login_name': row['login_name'],
+            'display_name': row['display_name'],
+            'password': row['password'],
+            'email': row['email'],
+            'must_change_password': row['must_change_password'],
+            'default_warehouse': row['default_warehouse'],
+            'default_role': row['default_role'],
+            'rsa_public_key': row['rsa_public_key'],
+            'rsa_public_key_2': row['rsa_public_key_2'],
+            }
+        user = {}
+        for key,value in temp.items():
+            if temp[key] != '' and temp[key] != None and temp[key] != None:
+                user[key] = value
+        users.append(user)
+
+    output_data = {'entries': users}
+    convert(output_data,'user')
+    
+def schema_yaml(df):
+    df = pd.DataFrame(df)
+    schemas = []
+
+    # Populate the list with dictionaries
+    for index, row in df.iterrows():
+        temp = {
+            'name': row['schema_name'],
+            'database' : row['Database_name'],
+            }
+        schema = {}
+        for key,value in temp.items():
+            if temp[key] != '' and temp[key] != None and temp[key] != None:
+                schema[key] = value
+        schemas.append(schema)
+
+    output_data = {'entries': schemas}
+    convert(output_data,'schema')
+
+def rm_yaml(df):
+    df = pd.DataFrame(df)
+    rms = []
+
+    # Populate the list with dictionaries
+    for index, row in df.iterrows():
+        temp = {
+            'name': row['resource_monitor_name'],
+            'creditQuota': row['creditQuota'],
+            'frequency': row['frequency'],
+            'START_TIMESTAMP' : row['START_TIMESTAMP']
+            }
+        rm = {}
+        for key,value in temp.items():
+            if temp[key] != '' and temp[key] != None and temp[key] != None:
+                rm[key] = value
+        rms.append(rm)
+
+    output_data = {'entries': rms}
+    convert(output_data,'resource_monitor')
+
+def role_yaml(df):
+    df = pd.DataFrame(df)
+    roles = []
+
+    # Populate the list with dictionaries
+    for index, row in df.iterrows():
+        temp = {
+            'name': row['role_name'],
+            }
+        role = {}
+        for key,value in temp.items():
+            if temp[key] != '' and temp[key] != None and temp[key] != None:
+                role[key] = value
+        roles.append(role)
+
+    output_data = {'entries': roles}
+    convert(output_data,'role')
+
+def privileges_yaml(df):
+    df = pd.DataFrame(df)
+    privileges = []
+
+    # Populate the list with dictionaries
+    for index, row in df.iterrows():
+        temp = {
+            'privilege_name': row['privilege'],
+            'objectType': row['objectType'],
+            'objectName': row['objectName'],
+            'roleName': row['roleName'],
+            'roleType': row['roleType'],
+            }
+        privilege = {}
+        for key,value in temp.items():
+            if temp[key] != '' and temp[key] != None and temp[key] != None:
+                privilege[key] = value
+        privileges.append(privilege)
+
+    output_data = {'entries': privileges}
+    convert(output_data,'privileges')
+
+def grantRole_yaml(df):
+    df = pd.DataFrame(df)
+    grantRoles = []
+    
+    # Populate the list with dictionaries
+    for index, row in df.iterrows():
+        print(row)
+        temp = {
+            'name': row['RoleName'],
+            'toRoles': row['RoleToAssign'],
+            'toUsers': row['UsersToAssign'],
+            }
+        grantRole = {}
+        for key,value in temp.items():
+            if temp[key] != '' and temp[key] != None and temp[key] != None:
+                grantRole[key] = value
+        grantRoles.append(grantRole)
+
+    output_data = {'entries': grantRoles}
+    convert(output_data,'grantRole')
+
+def file_format_yaml(df):
+    df = pd.DataFrame(df)
+    file_formats = []
+    
+    # Populate the list with dictionaries
+    for index, row in df.iterrows():
+        temp = {
+            'name': row['name'],
+            'database' : row['database'],
+            'schema' : row['schema'],
+            'compression': row['compression'],
+            'allow_duplicate': row['allow_duplicate'],
+            'date_format': row['date_format'],
+            'time_format': row['time_format'],
+            'timestamp_format': row['timestamp_format'],
+            'trim_space': row['trim_space'],
+            'enable_octal': row['enable_octal'],
+            'ignore_utf8_errors': row['ignore_utf8_errors'],
+            'skip_byte_order_mark': row['skip_byte_order_mark'],
+            'strip_outer_array': row['strip_outer_array'],
+            'fileType' : row['fileType'],
+            'binary_format': row['binary_format']
+            }
+        file_format = {}
+        for key,value in temp.items():
+            if temp[key] != '' and temp[key] != None and temp[key] != None:
+                file_format[key] = value
+        file_formats.append(file_format)
+
+    output_data = {'entries': file_formats}
+    convert(output_data,'fileFormats')
+    
+
+
+if __name__ == '__main__':
+    pass
