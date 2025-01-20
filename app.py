d8e6636bab66d7d46b9a874df7115ce09da7d35d
Hithesh Patel <hpatel@phdata.io>
2025-01-20 17:09:25 +0530

    updated UI

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import requests 
+import os
 from pandas import read_csv
 import time
 import json
@@ -13,6 +14,25 @@ from yaml_convertor import grantRole_yaml
 from yaml_convertor import rm_yaml
 import yaml
 import collections
+import zipfile
+from io import BytesIO
+
+# Function to create a ZIP file in memory
+def create_zip(folder_path):
+    buffer = BytesIO()
+    with zipfile.ZipFile(buffer, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                arcname = os.path.relpath(file_path, folder_path)
+                zipf.write(file_path, arcname)
+    buffer.seek(0)
+    return buffer
+
+# Path to the folder containing YAML files
+folder_path = "groups"
+
+
 
 st.set_page_config(page_title="Provision Tool", page_icon=":shield:",layout='wide')
 
@@ -22,26 +42,14 @@ st.markdown(f'<style>{css}</style>', unsafe_allow_html=True) #adding css to stre
 
 if 'project_name' not in st.session_state:
     st.session_state['project_name'] = True
-if 'standard_env' not in st.session_state:
-    st.session_state['standard_env'] = False
-if 'customized_env' not in st.session_state:
-    st.session_state['customized_env'] = False
 if 'domains' not in st.session_state:
     st.session_state['domains'] = False
 if 'envs' not in st.session_state:
     st.session_state['envs'] = False
-if 'warehouse_df' not in st.session_state:
-    st.session_state['warehouse_df'] = True
 if 'status' not in st.session_state:
     st.session_state['status'] = [False,False,False,False,False,False,False]
 if 'state' not in st.session_state:
     st.session_state['state'] = [False,False,False,False,False,False]
-if 'updated_df' not in st.session_state:
-    st.session_state['updated_df'] = [False,False,False,False,False,False]
-if 'user_object' not in st.session_state:
-    st.session_state['user_object'] = False
-if 'database_object' not in st.session_state:
-    st.session_state['database_object'] = False
 if "rows" not in st.session_state:
     st.session_state["rows"] = [{"user_name": "", "password": "", "roles": []}]
 if "yaml" not in st.session_state:
@@ -50,8 +58,8 @@ if "save_button" not in st.session_state:
     st.session_state['save_button'] = False
 if "warehouse" not in st.session_state:
     st.session_state["warehouse"] = [{"warehouse_name":"","warehouse_size":"","warehouse_type":"","initially_suspended":""}]
-if "init_spinner" not in st.session_state:
-    st.session_state["init_spinner"] = True
+if "project_setup_spinner" not in st.session_state:
+    st.session_state["project_setup_spinner"] = True
 if "warehouse_spinner" not in st.session_state:
     st.session_state["warehouse_spinner"] = True
 if "user_spinner" not in st.session_state:
@@ -60,6 +68,8 @@ if "role" not in st.session_state:
     st.session_state["role"] = [{"role_name":""}]
 if "role_assign_user" not in st.session_state:
     st.session_state["role_assign_user"] = [{"Select_user":"","Select_role":""}]
+if "project_setup_spinner_check" not in st.session_state:
+    st.session_state["project_setup_spinner_check"] = True
 
 
 def add(resource,value):
@@ -93,21 +103,19 @@ def main():
         env_list = st.pills(label="Please select the environments required",selection_mode="multi",options=["PROD","DEV","QA","NONPROD","SANDBOX"],disabled= st.session_state['envs'],default=None)
 
         st.divider()
-        if not project_name or not domain_name or not env_list:
-            st.session_state["init_spinner"] = True
-        if st.session_state["init_spinner"]:
+        if st.session_state["project_setup_spinner"]:
             my_bar = st.progress(0, text="")
-        if project_name and st.session_state["init_spinner"]:
+        if project_name and st.session_state["project_setup_spinner"]:
             my_bar.progress(40,text="")
-        if domain_name and st.session_state["init_spinner"]:
+        if domain_name and st.session_state["project_setup_spinner"]:
             my_bar.progress(75,text="")
-        if project_name and domain_name and env_list and st.session_state["init_spinner"]:
+        if project_name and domain_name and env_list:
             my_bar.progress(100,text="")
-            with st.spinner('In progress...'):
-                time.sleep(3)
-            st.session_state['status'][0] = True
-            st.session_state['warehouse_df'] = False
-            st.session_state["init_spinner"] = False
+            if st.session_state["project_setup_spinner_check"]:
+                with st.spinner('In progress...'):
+                    time.sleep(3)
+                st.session_state["project_setup_spinner_check"] = False
+                st.session_state['status'][0] = True
 
     with st.status(label="Specify the warehouse to create.",expanded=st.session_state['status'][0],state='complete' if st.session_state['state'][0] else 'error') as warehouse_container:
         warehouse = []
@@ -118,7 +126,7 @@ def main():
                     if domain_name:
                         row["warehouse_name"] = st.text_input(
                             label=f"Warehouse Name",
-                            value=(f"{domain_name}_Adhoc_wh").upper(),
+                            # value=(f"{domain_name}_Adhoc_wh").upper(),
                             placeholder=(f"{domain_name}_Adhoc_wh").upper(),
                             key=f"warehouse_name_{index}"
                         )
@@ -135,7 +143,6 @@ def main():
                         # value=row["warehouse_size"],
                         options = ["X-Small","Small","Medium","Large","X-Large","2X-Large","3X-Large","4X-Large","5X-Large","6X-Large",],
                         placeholder="X-Small",
-                        # default = "X-Small",
                         key=f"warehouse_size_{index}",
                     )
                 with cols[2]:
@@ -150,8 +157,6 @@ def main():
                     row["initially_suspended"] = st.selectbox(
                         label=f"Initially Suspended",
                         options=["True","False"],
-                        # placeholder="STANDARD",
-                        # default= "STANDARD",
                         key=f"initially_suspended_{index}"
                     )
                 with cols[4]:
@@ -172,6 +177,7 @@ def main():
         # resource monitor
         rm_required = st.checkbox(label="Do you need resource monitor?")
         rm_name,rm_monitor_type,rm_frequency,rm_notify,rm_notify_suspend,rm_notify_only= "","","","","",""
+        rm_creditQuota = ""
         if rm_required: 
             rm_name = st.text_input(label = "Resource Monitor Name",placeholder="load_monitor",key="resource_monitor")
             rm_monitor_type = st.pills(label = "Monitor Type",options=['Account','Warehouse'],key="monitor_type")
@@ -185,7 +191,7 @@ def main():
             rm_notify_only = st.checkbox(label = "Notify at 95%")
 
         st.divider()
-
+     
         if warehouse[0][0] and st.session_state["warehouse_spinner"]:
             with st.spinner("In progress..."):
                 time.sleep(5)
@@ -219,7 +225,7 @@ def main():
                 with cols[2]:
                     role_list = ["SYSADMIN", "SECURITYADMIN", "USERADMIN","ACCOUNTADMIN","PUBLIC"] 
                     row["roles"] = st.multiselect(
-                        label=f"System Defined Roles",
+                        label=f"System Defined Roles (Optional)",
                         options=role_list,
                         key=f"roles_{index}"
                     )
@@ -252,7 +258,7 @@ def main():
             st.session_state['status'][2] = True
     
     with st.status(label="Specify the roles to create.",expanded=st.session_state['status'][2],state='complete' if st.session_state['state'][2] else 'error') as roles_container:
-        
+        init_role_name = st.text_input("Role Name",placeholder="Analyst_Viewer",key = "role_name")
         rw_ro = st.checkbox(label="Do you need database level RO and RW roles?",key="rw_ro",help="Roles will be created as <Domain_name>_<ENV>_<RO>,<Domain_name>_<ENV>_<RW>")
         roles_list = {"role_name":[]}
         rw_ro_list = ["RW","RO"]
@@ -261,7 +267,10 @@ def main():
             for env in env_list:
                     print("in line 373")
                     for value in rw_ro_list:
-                        role_name =  domain_name.upper() + '_' + env + '_' + value 
+                        if init_role_name:
+                            role_name = init_role_name + "_" + domain_name.upper() + '_' + env + '_' + value 
+                        else:
+                            role_name =  domain_name.upper() + '_' + env + '_' + value 
                         roles_list["role_name"].append(role_name)
                 
         else:
@@ -298,98 +307,98 @@ def main():
         render_rows()
        
         # print("line 298",role_assign_user,"userlist",user["temp"][2]+role_assign_user["temp"])
-        
-        if st.button("Save Data",disabled=st.session_state['save_button']):
-            st.session_state['status'][2] = False
-            roles_container.update(expanded=st.session_state['status'][2],state='complete')
-            st.session_state['state'][2] = True
-            st.session_state['status'][3] = True
-            print("line 298",roles_list)
-            snowflake_config = {
-                "Snowflake": {
-                    "ProjectName": project_name,
-                    "user": [
-                        {
-                            "user_name": key,
-                            "password": user[key][1],
-                            "roles_to_assign": user[key][2] + role_assign_user[key][0]
-                        } for key,items in user.items()
-                    ],
-                    "warehouse": [
-                        {
-                            "warehouse_name":w[0],
-                            "warehouse_size":w[1],
-                            "warehouse_type":w[2],
-                            "initially_suspended":str(w[3])
-                        } for w in warehouse
-                    ],
-                    "resource_monitor": {
-                        "resource_monitor_name":"" if not rm_name else rm_name,
-                        "frequency":"" if not rm_frequency else rm_frequency,
-                        "monitor_level":"" if not rm_monitor_type else rm_monitor_type,
-                        "trigger": {
-                            "notify" :"" if not rm_notify else rm_notify,
-                            "notify_suspend":"" if not rm_notify_suspend else rm_notify_suspend,
-                            "notify_only":"" if not rm_notify_only else rm_notify_only
-                        }
-                    },
-                    "env": env_list,
-                    "Domains": domain_name,
-                    "roles": roles_list["role_name"],
-                    "assign_privileges_to_role": [
-                        {
-                            "object_name": (
-                                "PROD" if "_PROD_" in roles_list["role_name"][i] else
-                                "DEV" if "_DEV_" in roles_list["role_name"][i] else
-                                "QA" if "_QA_" in roles_list["role_name"][i] else
-                                "SANDBOX"
-                            ),
-                            "object_type": "Database",
-                            "role_name": roles_list["role_name"][i] ,
-                            "privilege": (
-                                "usage" if "_RO" in roles_list["role_name"][i] else
-                                ["usage","modify"] 
-                             ) 
-                        } for i in range(len(roles_list["role_name"]))
-                    ],
-                    "assign_role_to_user":[
-                        {
-                            "role_name": role_assign_user[key][0],
-                            "to_user": key
-                        } for key,value in role_assign_user.items() 
-                    ],
-                    "resource_monitor":[
-                        {
-                        "rm_name": rm_name,
-                        "rm_frequency": rm_frequency,
-                        "rm_type": rm_monitor_type,
-                        "rm_notify": rm_notify,
-                        "rm_notify_suspend": rm_notify_suspend,
-                        "rm_notify_only": rm_notify_only,
-                        "creditQuota": int(rm_creditQuota)
+    st.divider()
+
+    if st.button("Save Data",disabled=st.session_state['save_button']):
+        st.session_state['status'][2] = False
+        roles_container.update(expanded=st.session_state['status'][2],state='complete')
+        st.session_state['state'][2] = True
+        st.session_state['status'][3] = True
+        print("line 298",len(role_assign_user),len(user),list(user.keys()),role_assign_user )
+        snowflake_config = {
+            "Snowflake": {
+                "ProjectName": project_name,
+                "user": [
+                    {
+                        "user_name": key,
+                        "password": user[key][1],
+                        "roles_to_assign": ["hh"] if len(list(user.items())) == 1 and len(role_assign_user) == 0  else user[key][2] + role_assign_user[key][0]
+                    } for key,items in user.items()
+                ],
+                "warehouse": [
+                    {
+                        "warehouse_name":w[0],
+                        "warehouse_size":w[1],
+                        "warehouse_type":w[2],
+                        "initially_suspended":str(w[3])
+                    } for w in warehouse
+                ],
+                "resource_monitor": {
+                    "resource_monitor_name":"" if not rm_name else rm_name,
+                    "frequency":"" if not rm_frequency else rm_frequency,
+                    "monitor_level":"" if not rm_monitor_type else rm_monitor_type,
+                    "trigger": {
+                        "notify" :"" if not rm_notify else rm_notify,
+                        "notify_suspend":"" if not rm_notify_suspend else rm_notify_suspend,
+                        "notify_only":"" if not rm_notify_only else rm_notify_only
                     }
-                    ]
-                    
+                },
+                "env": env_list,
+                "Domains": domain_name,
+                "roles": roles_list["role_name"],
+                "assign_privileges_to_role": [
+                    {
+                        "object_name": (
+                            "PROD" if "_PROD_" in roles_list["role_name"][i] else
+                            "DEV" if "_DEV_" in roles_list["role_name"][i] else
+                            "QA" if "_QA_" in roles_list["role_name"][i] else
+                            "SANDBOX"
+                        ),
+                        "object_type": "Database",
+                        "role_name": roles_list["role_name"][i] ,
+                        "privilege": (
+                            "usage" if "_RO" in roles_list["role_name"][i] else
+                            ["ALL PRIVILEGES"] 
+                            ) 
+                    } for i in range(len(roles_list["role_name"]))
+                ],
+                "assign_role_to_user":[
+                    {
+                        "role_name": role_assign_user[key][0],
+                        "to_user": key
+                    } for key,value in role_assign_user.items() 
+                ],
+                "resource_monitor":[
+                    {
+                    "rm_name": rm_name,
+                    "rm_frequency": rm_frequency,
+                    "rm_type": rm_monitor_type,
+                    "rm_notify": rm_notify,
+                    "rm_notify_suspend": rm_notify_suspend,
+                    "rm_notify_only": rm_notify_only,
+                    "creditQuota": "" if not rm_creditQuota else int(rm_creditQuota)
                 }
+                ]
+                
             }
+        }
 
-            with open("output.json", "w") as json_file:
-                json.dump(snowflake_config, json_file, indent=2)
+        with open("output.json", "w") as json_file:
+            json.dump(snowflake_config, json_file, indent=2)
 
-        
-    with st.expander(label="Review",expanded= st.session_state['status'][3]) as review:
+     
+    with st.expander(label="Review Data",expanded= st.session_state['status'][3]) as review:
         with open("output.json","r") as file:
             data = json.load(file)
         st.json(data,expanded=True)
 
-    if st.button("Generate",key="json_to_ymal",disabled=st.session_state["save_button"]):
-#         st.session_state["yaml"]  = True
-#         pass    
-# if st.session_state["yaml"]:
+    st.divider()
+
+    if st.button("Generate Yaml",key="json_to_ymal",disabled=st.session_state["save_button"]):
 
         # display yaml file code 
         url = "git clone https://hiteshp__h__1334_-admin@bitbucket.org/phdata/provision_tool_test_repo.git"
-        st.write("Use this URL to clone Provision Tool Repository and paste the below generated yaml code in group section files")
+        st.caption("Note: Use the below command to clone Provision Tool Repository and paste the below generated yaml code in group section files")
         st.code(url,language="git")
         st.write("warehouse.yaml")
         warehouse_yaml()
@@ -432,6 +441,16 @@ def main():
         with open("groups\\resource_monitor.yaml") as file:
             yaml_data = file.read()
         st.code(yaml_data,language='yaml',wrap_lines=True,line_numbers=True)
+
+        zip_buffer = create_zip(folder_path)
+
+        # Provide a download button
+        st.download_button(
+            label="Download files",
+            data=zip_buffer,
+            file_name="groups.zip",
+            mime="application/zip",
+        )
 
 if __name__ == '__main__':
     main()
